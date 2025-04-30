fbf59364de0fef3164685c24a49b948c110e346a
Mary Habibi <mary.habibi@iqdigital.de>
2025-04-30 12:32:43 +0200

    fix: remove outdated parameter from ad request for improved ad loading

--- script.js
+++ script.js
@@ -165,7 +165,6 @@ function initializeIMA() {
   "&cust_params=kw%3Dmary_testplayer" +
   "&ad_rule=1" +
   "&tfcd=0" +
-  "&pmad=2" +
   "&pos=pre" +
   "&npa=0" +
   "&sz=16x9%7C480x360%7C640x360%7C640x480" +
