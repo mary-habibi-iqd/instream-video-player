8698062820182e94c6a81f7e6684279ec91b513d
Mary Habibi <mary.habibi@iqdigital.de>
2025-05-04 16:09:16 +0200

    feat: Configure Webpack for module bundling and HTML generation

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: './',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Digital Clock',
+            template: 'src/index.html',
+        }),
+    ],
+};
